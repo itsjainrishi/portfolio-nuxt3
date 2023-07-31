e32908e2c05579074d27ca85f15e19336b2d5233
Rishi Jain <rishi.jaipur17@gmail.com>
2023-08-01 03:34:00 +0530

    add cryptoland

---
+++ content/funprojects/cryptoland.md
@@ -0,0 +1,13 @@
+---
+active: true
+title: 'CryptoLand'
+github: 'https://github.com/itsjainrishi/cryptoland'
+external: 'http://cryptoland.netlify.app'
+image: 'cryptoland.webp'
+tech:
+  - React
+  - Tailwind
+  - CoinGecko-API
+---
+
+A cryptoexchange which allows user access to information about cryptocurrencies.
--- pages/work.vue
+++ pages/work.vue
@@ -1,18 +1,32 @@
 <script setup>
-const { data: funProjects } = await useAsyncData('work', () => queryContent('funprojects').find());
+const { data: funProjects } = await useAsyncData('fwork', () => queryContent('funprojects').where({ active: true }).find());
 const { data: projects } = await useAsyncData('work', () => queryContent('projects').find());
+console.log(funProjects)
 </script>
 
 <template>
   <div class="container p-4 lg:py-10">
     <section class="projects-container">
-      <h3 class="heading text-3xl xl:text-4xl text-secondary mb-8 lg:mb-20">Some Things I've Built</h3>
-      <div
-        v-for="project in projects"
-        :key="project.title"
-        class="mb-20"
-      >
-        <project-component :item="project" />
+      <div>
+        <h3 class="heading text-3xl xl:text-4xl text-secondary mb-8 lg:mb-20">Some Things I've Built</h3>
+        <div
+          v-for="project in projects"
+          :key="project.title"
+          class="mb-20"
+        >
+          <project-component :item="project" />
+        </div>
+      </div>
+
+      <div class="side-projects mt-28">
+        <h3 class="heading text-3xl xl:text-4xl text-secondary mb-8 lg:mb-20">Side Projects</h3>
+        <div
+          v-for="project in funProjects"
+          :key="project.title"
+          class="mb-20"
+        >
+          <project-component :item="project" />
+        </div>
       </div>
     </section>
   </div>
